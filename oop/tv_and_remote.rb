10a965e2b1d75d48623c86977b9a371b57a58376
O'Hoon Kwon <ohkwon@ucdavis.edu>
2016-07-29 18:08:04 -0700

    solve tv and remote oop problem

--- oop/tv_and_remote.rb
+++ oop/tv_and_remote.rb
@@ -10,3 +10,79 @@
 # increment_volume (this will increase the tv's volume by 1)
 # decrement_volume (this will decrease the tv's volume by 1)
 # set_channel (this will change the tv's channel to whatever integer is passed to this method)
+
+class Tv
+
+  attr_accessor :power, :volume, :channel
+
+  def initialize
+    @power = false
+    @volume = 0
+    @channel = 1
+  end
+
+end
+
+class Remote
+
+  def initialize(choose_tv)
+    @tv = choose_tv
+  end
+
+  def toggle_power
+    @tv.power = !@tv.power
+  end
+
+  def increment_volume
+    @tv.volume += 1
+  end
+
+  def decrement_volume
+    @tv.volume -= 1
+  end
+
+  def set_channel(channel)
+    @tv.channel = channel
+  end
+
+end
+
+home_tv = Tv.new
+
+home_remote = Remote.new(home_tv)
+
+home_remote.toggle_power
+puts home_tv.power
+
+if home_tv.power
+  puts "PASS!!!"
+else
+  puts "F!!!!!"
+end
+
+home_remote.increment_volume
+puts home_tv.volume
+
+if home_tv.volume == 1
+  puts "PASS!!!"
+else
+  puts "F!!!!!"
+end
+
+home_remote.decrement_volume
+puts home_tv.volume
+
+if home_tv.volume == 0
+  puts "PASS!!!"
+else
+  puts "F!!!!!"
+end
+
+home_remote.set_channel(82)
+puts home_tv.channel
+
+if home_tv.channel == 82
+  puts "PASS!!!"
+else
+  puts "F!!!!!"
+end
